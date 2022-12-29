c0c969cfd336a6973fccc969423a49ff9d62baa8
aiiiden <hjjam100@gmail.com>
2022-12-29 17:17:31 +0900

    Add Aptos wallet connect

--- features/wallet/pages/WalletConnect.tsx
+++ features/wallet/pages/WalletConnect.tsx
@@ -1,3 +1,5 @@
+import useAptos from "../hooks/useAptos";
+
 export const getServerSideProps = async () => {
     return {
         props: {},
@@ -5,13 +7,24 @@ export const getServerSideProps = async () => {
 };
 
 export default function WalletConnectPage() {
+    const { account, getAdress, disconnect, connected } = useAptos();
     return (
         <div className="bg-gray-200 w-full h-screen flex justify-center items-center">
-            <div className="bg-white border-2 border-gray-400 rounded p-8">
+            <div className="bg-white border-2 border-gray-400 rounded p-8 w-[320px]">
                 <h1 className="text-2xl font-bold mb-4">
                     Connect Aptos Wallet
                 </h1>
-                <p>wassup?</p>
+
+                <div>
+                    <button
+                        className="bg-black text-white rounded px-4 py-2"
+                        onClick={!connected ? getAdress : disconnect}
+                    >
+                        {!connected ? "Connect" : "Disconnect"} Wallet
+                    </button>
+                </div>
+
+                <p className="break-all text-sm">{account}</p>
             </div>
         </div>
     );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,27 +2,40 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ToastContainer } from "react-toastify";
+
+import {
+    WalletProvider,
+    MartianWalletAdapter,
+} from "@manahippo/aptos-wallet-adapter";
+
+import "react-toastify/dist/ReactToastify.css";
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: true,
-      retry: 1,
-      staleTime: 1000 * 5,
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: true,
+            retry: 1,
+            staleTime: 1000 * 5,
+        },
     },
-  },
 });
 
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <RecoilRoot>
-        <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
-        </QueryClientProvider>
-      </RecoilRoot>
-    </>
-  );
+    return (
+        <WalletProvider
+            wallets={[new MartianWalletAdapter()]}
+            autoConnect={false}
+            onError={(error) => console.error(error)}
+        >
+            <RecoilRoot>
+                <QueryClientProvider client={queryClient}>
+                    <ToastContainer />
+                    <Component {...pageProps} />
+                </QueryClientProvider>
+            </RecoilRoot>
+        </WalletProvider>
+    );
 }
 
 export default App;
